c0c4384de1eef50ccf934f7e7212632e7eb02c56
blofland <84938746+blofland@users.noreply.github.com>
2021-12-12 20:13:41 -0700

    Update game.js with QuestionView

--- src/components/game.js
+++ src/components/game.js
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import questions from "./questions";
+import QuestionView from "./question-view";
+
 export default function Game(){
   const [timer, setTimer] = useState(timeout)
+  const [questionIndex, setIndex] = useState(0)
   useEffect(()=>{
         // Countdown Timer
         if(!locked && timer > 0) setTimeout(()=>{
             setTimer(timer - 1)
         },1000)
     }, [timer, setTimer])
+  
+  return <QuestionView question={questions[questionIndex]} />
 }
