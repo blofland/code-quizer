37cea8f8d9b314b1f5d1ea2bfb6905796121858e
blofland <84938746+blofland@users.noreply.github.com>
2021-12-12 20:07:20 -0700

    Added the LandingPage Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { Container } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
+import LandingPage from './components/landing-page';
 
 function App() {
   return (
     <Container sx={{height: '100vh'}}>
       <Routes>
-        <Route path="/"/>
+        <Route path="/" element={LandingPage}/>
         <Route path="/game"/>
         <Route path="/scores"/>
       </Routes>
