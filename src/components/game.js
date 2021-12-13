0d1678a956d0dd3294540cbd2f92f6cc118db1a7
blofland <84938746+blofland@users.noreply.github.com>
2021-12-12 20:09:35 -0700

    Created Countdown Timer

--- src/components/game.js
+++ src/components/game.js
@@ -1,3 +1,10 @@
+import { useEffect, useState } from "react";
 export default function Game(){
-
+  const [timer, setTimer] = useState(timeout)
+  useEffect(()=>{
+        // Countdown Timer
+        if(!locked && timer > 0) setTimeout(()=>{
+            setTimer(timer - 1)
+        },1000)
+    }, [timer, setTimer])
 }
